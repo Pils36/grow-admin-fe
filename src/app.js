5aa72396fa261a05c265d9f6394b899e52a3398a
Adebambo Adenuga <adenugaadebambo411@gmail.com>
2022-02-01 19:41:33 +0100

    continued integration

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/auth'));
+app.use('/', require('./routes/welcome'));
+app.use('/auth', require('./routes/auth'));
 app.use('/dashboard', require('./routes/home'));
+app.use('*', require('./routes/error'));
 
 app.listen(PORT, () => console.log(`listening on port ${process.env.APP_URL}:${PORT}`));
--- src/routes/home.js
+++ src/routes/home.js
@@ -5,5 +5,66 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	res.render('./pages/home/index');
 });
+router.get('/earnings', (req, res) => {
+	res.render('./pages/home/earnings');
+});
+router.get('/analytics', (req, res) => {
+	res.render('./pages/home/analytics');
+});
+router.get('/fans', (req, res) => {
+	res.render('./pages/home/fans');
+});
+router.get('/celebs', (req, res) => {
+	res.render('./pages/home/celebs');
+});
+router.get('/acceptedbookings', (req, res) => {
+	res.render('./pages/home/acceptedbookings');
+});
+router.get('/pendingbookings', (req, res) => {
+	res.render('./pages/home/pendingbookings');
+});
+router.get('/declinedbookings', (req, res) => {
+	res.render('./pages/home/declinedbookings');
+});
+
+router.get('/celebearnings', (req, res) => {
+	res.render('./pages/home/celebrityearning');
+});
+
+router.get('/becueddeduction', (req, res) => {
+	res.render('./pages/home/becueddeduction');
+});
+
+router.get('/transactionhistory', (req, res) => {
+	res.render('./pages/home/transactionhistory');
+});
+
+router.get('/celebbanks', (req, res) => {
+	res.render('./pages/home/celebbanks');
+});
+
+router.get('/addedcards', (req, res) => {
+	res.render('./pages/home/addedcards');
+});
+
+router.get('/withdrawalrequest', (req, res) => {
+	res.render('./pages/home/withdrawalrequest');
+});
+
+router.get('/withdrawalprocessed', (req, res) => {
+	res.render('./pages/home/withdrawalprocessed');
+});
+
+router.get('/waitlist', (req, res) => {
+	res.render('./pages/home/waitlist');
+});
+
+router.get('/talents', (req, res) => {
+	res.render('./pages/home/talents');
+});
+
+router.get('/partners', (req, res) => {
+	res.render('./pages/home/partners');
+});
 
 module.exports = router;

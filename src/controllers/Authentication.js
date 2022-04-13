8e12299e7d84048bf408b0c25dc84ac304887c68
Adebambo Adenuga <adenugaadebambo41@gmail.com>
2022-04-13 11:43:49 +0100

    forgot password and reset password

--- src/controllers/Authentication.js
+++ src/controllers/Authentication.js
@@ -16,6 +16,14 @@ const register = (req, res) => {
 	res.render('./pages/auth/register');
 };
 
+const forgotPassword = (req, res) => {
+	res.render('./pages/auth/forgot-password');
+};
+
+const resetPassword = (req, res) => {
+	res.render('./pages/auth/reset-password');
+};
+
 const loginAdmin = async (req, res) => {
 	try {
 		data = req.body;
@@ -62,4 +70,72 @@ const adminLogout = async (req, res) => {
 	}
 };
 
-module.exports = { login, register, loginAdmin, adminLogout };
+const postForgotPassword = async (req, res) => {
+	try {
+		data = req.body;
+		data.link = `${req.headers.origin}/auth/reset-password`;
+
+		const response = await axios({
+			method: 'POST',
+			url: `${process.env.APP_ENV == 'local'
+				? process.env.ENDPOINT_URL_DEV
+				: process.env.ENDPOINT_URL_PROD}/auth/forgot-password`,
+			headers: headers,
+			data: data
+		});
+
+		res.status(response.status).json({
+			data: response.data.data,
+			message: response.data.message
+		});
+	} catch (error) {
+		res.status(401).json({
+			data: [],
+			message: error.message
+		});
+	}
+};
+
+const postResetPassword = async (req, res) => {
+	try {
+		var userkey = req.headers.referer.split('=')[1];
+		data = req.body;
+		data.email = atob(userkey);
+
+		if (req.body.new_password != req.body.confirm_password)
+			res.status(400).json({
+				data: [],
+				message: 'Password does not match'
+			});
+
+		const response = await axios({
+			method: 'POST',
+			url: `${process.env.APP_ENV == 'local'
+				? process.env.ENDPOINT_URL_DEV
+				: process.env.ENDPOINT_URL_PROD}/auth/reset-password`,
+			headers: headers,
+			data: data
+		});
+
+		res.status(response.status).json({
+			data: response.data.data,
+			message: response.data.message
+		});
+	} catch (error) {
+		res.status(401).json({
+			data: [],
+			message: error.message
+		});
+	}
+};
+
+module.exports = {
+	login,
+	register,
+	forgotPassword,
+	resetPassword,
+	loginAdmin,
+	adminLogout,
+	postForgotPassword,
+	postResetPassword
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,13 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
-const { login, register, loginAdmin, adminLogout } = require('../controllers/Authentication');
+const {
+	login,
+	register,
+	forgotPassword,
+	resetPassword,
+	loginAdmin,
+	adminLogout,
+	postForgotPassword,
+	postResetPassword
+} = require('../controllers/Authentication');
 
 router.get('/login', login);
 
 router.get('/register', register);
 
+router.get('/forgot-password', forgotPassword);
+router.get('/reset-password', resetPassword);
+
 router.post('/login-admin', loginAdmin);
 router.post('/logout-admin', adminLogout);
+router.post('/forgotpassword', postForgotPassword);
+router.post('/resetpassword', postResetPassword);
 
 module.exports = router;
